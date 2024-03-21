f5ce48b23a5fe8bb619ddbbd06a51c14dba9e877
gogicha007 <gogicha@gmail.com>
2024-03-21 22:41:05 +0400

    feat: 13 tasks done

--- test.js
+++ test.js
@@ -1,24 +1,28 @@
-function getWeekNumberByDate(date) {
-  console.log(date)
-  const weekdays = [
-    'sun',
-    'monday',
-    'tuesday',
-    'wen',
-    'thu',
-    'fri',
-    'sat',
-  ]
-  const wDay = 1 // for monday
-  const startOfYear = new Date(date.getFullYear(), 0, 1)
-  const firstMonday = new Date(date.getFullYear(), 0, 1)
-  firstMonday.setDate(firstMonday.getDate() + (((wDay + 7 - firstMonday.getDay()) % 7) || 7))
-  console.log(startOfYear)
-  console.log(firstMonday)
-  const diff = Math.ceil((date - firstMonday) / (24 * 3600 * 1000) / 7)
-  console.log(startOfYear < firstMonday)
-  return diff + (startOfYear < firstMonday ? 1 : 0)
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const start = new Date(period.start.split('-').reverse().join('-'));
+  const end = new Date(period.end.split('-').reverse().join('-'));
+
+  let theDate = start;
+  let year = 0;
+  let month = 0;
+  let day = 0;
+  let stepWork = countWorkDays;
+  let step = 0;
+  const datesArr = [];
+  while (theDate <= end) {
+    year = theDate.getFullYear();
+    month =
+      theDate.getMonth() < 10
+        ? `0${theDate.getMonth() + 1}`
+        : theDate.getMonth() + 1;
+    day = theDate.getDate() < 10 ? `0${theDate.getDate()}` : theDate.getDate();
+    datesArr.push(`${day}-${month}-${year}`);
+    stepWork -= 1;
+    step = stepWork !== 0 ? 1 : countOffDays+1;
+    theDate.setDate(theDate.getDate() + step);
+    stepWork = stepWork === 0 ? countWorkDays : stepWork;
+  }
+  return datesArr;
 }
 
-
-console.log(getWeekNumberByDate(new Date(2017, 7, 21)));
+console.log(getWorkSchedule({ start: '01-01-2024', end: '10-01-2024' }, 1, 1));
